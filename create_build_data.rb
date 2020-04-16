36db2df0e97d6a2cdbc73abd81db01503bdcd09a
Alan Brockett <abrockett@gmail.com>
2020-04-15 22:05:40 -0600

    Updating create_build_data

--- create_build_data.rb
+++ create_build_data.rb
@@ -2,12 +2,13 @@
 
 require 'rally_api'
 require 'pp'
+require 'date'
 
 @config = {:base_url => "https://abrockett.testn.f4tech.com/slm"}
 @config[:username]   = "abrockett@rallydev.com"
 @config[:password]   = "Password"
 @config[:workspace]  = "Rally"
-@config[:project]    = "Hellfish"
+@config[:project]    = "Tally"
 
 def show_some_values(title, builddef)
   values = ["Name", "Description", "Project", "CreationDate"]
@@ -20,7 +21,7 @@ def show_some_values(title, builddef)
   end
 end
 
-def find_build_defs(rally)
+def find_build_defs()
   query = RallyAPI::RallyQuery.new()
   query.type = "builddefinition"
   query.fetch = "Name,CreationDate,Description,Project"
@@ -31,24 +32,46 @@ def find_build_defs(rally)
   query.order = "CreationDate Desc"
   query.query_string = "(Name = \"Hellfish\")"
 
-  results = rally.find(query)
+  results = @rally_api.find(query)
   results.each do |result|
     show_some_values("Result", result)
     puts result.ObjectID
   end
 end
 
-begin
-  rally = RallyAPI::RallyRestJson.new(@config)
-
+def create_build_defs
   fields = {}
   fields["Name"] = "Tally Builds"
   fields["Description"] = "Builds for Tally"
+  fields["Project"] = @project
 
-  new_builddef = rally.create("builddefinition", fields)
+  new_builddef = @rally_api.create("builddefinition", fields)
   show_some_values("Created new build definition", new_builddef)
 
-  find_build_defs(rally)
+  return new_builddef
+end
+
+def create_builds(builddef)
+  fields = {}
+  fields["BuildDefinition"] = builddef
+  fields["Duration"] = 1.5
+  fields["Start"] = DateTime.new(2019, 8, 10, 4, 10, 9).iso8601
+  fields["Status"] = "SUCCESS"
+
+  new_build = @rally_api.create("build", fields)
+  puts new_build.inspect
+end
+
+begin
+  @rally_api = RallyAPI::RallyRestJson.new(@config)
+  @workspace = @rally_api.find_workspace(@config[:workspace])
+  @project   = @rally_api.find_project(@workspace, @config[:project])
+  puts @workspace._ref
+  puts @project._ref
+
+  builddef = create_build_defs
+  create_builds(builddef)
+  find_build_defs
 
 rescue Exception => boom
   puts "Rescued #{boom.class}"
